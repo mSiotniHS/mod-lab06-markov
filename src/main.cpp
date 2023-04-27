4d26efc66f627a57b11b00828ca893fae0c4b067
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-27 16:26:03 +0300

    [App] Написал пример программы

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include "textgen.h"
+
+int main() {
+    std::random_device rd;
+    TextGenerator generator(2, rd());
+
+    std::string input;
+    std::ifstream inputFile("input.txt");
+    std::string fileLine;
+    while (inputFile >> fileLine) {
+        input.append(fileLine + ' ');
+    }
+    inputFile.close();
+
+    generator.analyze(input);
+    std::string text = generator.generateText(1000, { "Одна", "девочка" });
+
+    std::ofstream outputFile("output.txt");
+    outputFile << text;
+    outputFile.close();
+
+    return 0;
+}
